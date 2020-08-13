9914959b544d17646e9d75d7190527483175e7f6
Andrius Simanaitis <eragonas5@gmail.com>
2020-08-13 11:28:49 +0300

    added firebase and registration

--- client/src/signup.js
+++ client/src/signup.js
@@ -15,14 +15,53 @@ const signUpForm = () => {
   const element = document.createElement('main')
 
   element.innerHTML += `<div class="signUpForm">
-    <div>Hello, I bet you want to register bla bla bla</div>
-    <div class="inputElement"><input type="email" class="inputElement-input" placeholder="email" /></div><br />
-    <div class="inputElement"><input type="password" class="inputElement-input" placeholder="password" /></div><br />
-    <div class="inputElement"><input type="password" class="inputElement-input" placeholder="repeat password" /></div><br />
-    <div class="inputElement"><button class="inputElement-button" onclick="window.location = 'browse.html'">Register</button></div>
+    <div>Register and enjoy your stay!</div>
+    <div id="error"></div>
+    <form action="#" id="form">
+      <div class="inputElement"><input type="email" id="email" class="inputElement-input" placeholder="email" /></div><br />
+      <div class="inputElement"><input type="password" id="pass1" class="inputElement-input" placeholder="password" /></div><br />
+      <div class="inputElement"><input type="password" id="pass2" class="inputElement-input" placeholder="repeat password" /></div><br />
+      <div class="inputElement"><button class="inputElement-button" onclick="register()">Register</button></div>
+    </form>
   </div>`
+  return element
+}
+
+const script = () => {
+  const element = document.createElement('script')
+  element.innerHTML = `
+setInterval(() => {  
+  if(firebase.auth().currentUser !== null){
+    window.location = 'browse.html'
+  }
+}, 500)
+const form = document.getElementById('form')
+const errorDiv = document.getElementById('error')
+form.addEventListener('submit', () => {
+  event.preventDefault();
+}) 
+const register = async () => {
+  if(form.elements[1].value !== form.elements[2].value){
+    errorDiv.innerHTML = 'Passwords must match'
+    return
+  }
+  errorDiv.innerHTML = ''
+  let signUp = {}
+  try {
+    signUp = await firebase.auth().createUserWithEmailAndPassword(form[0].value, form[1].value)
+  } catch(e) {
+    signUp = e
+  }
+  if((signUp.user || '') === ''){
+    errorDiv.innerHTML = signUp.message || 'Error'
+  }else{
+    window.location = 'browse.html'
+  }
+} 
+`
   return element
 }
 
 document.body.appendChild(header())
 document.body.appendChild(signUpForm())
+document.body.appendChild(script())
