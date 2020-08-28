106faae64528b4ff9daffd354bb2045e8ad34899
Andrius Simanaitis <eragonas5@gmail.com>
2020-08-28 11:01:27 +0300

    ported header

---
+++ react-client/src/Components/Header.tsx
@@ -0,0 +1,211 @@
+import * as React from 'react'
+import Dialog from './Dialog'
+import { Link } from 'react-router-dom'
+
+const Login = (props: { Ref: React.RefObject<HTMLDialogElement> }) => {
+  const FormRef = React.useRef<HTMLFormElement>(null)
+  const ErrorRef = React.useRef<HTMLDivElement>(null)
+  React.useEffect(() => {
+    if (FormRef.current !== null) {
+      console.log('pavyko', FormRef.current)
+      FormRef.current.addEventListener('submit', (e) => {
+        e.preventDefault()
+        handleLogin()
+      })
+    }
+  }, [])
+  const handleLogin = () => {
+    if (FormRef.current !== null && ErrorRef.current !== null) {
+      console.log('blet')
+      const form = (FormRef.current as unknown) as HTMLInputElement[]
+      const errorDiv = ErrorRef.current
+      errorDiv.innerHTML = 'Loging in, please wait!'
+      ;(async () => {
+        let signUp = { user: null, message: null }
+
+        try {
+          //@ts-ignore
+          signUp = await firebase
+            .auth()
+            .signInWithEmailAndPassword(form[0].value, form[1].value)
+        } catch (e) {
+          signUp = e
+        }
+        if ((signUp.user || '') === '') {
+          errorDiv.innerHTML = signUp.message || 'Error'
+        } else {
+          document.location.reload()
+        }
+      })()
+    }
+  }
+  return (
+    <Dialog title="Login" Ref={props.Ref}>
+      <div className="signUpForm">
+        <h3>Login</h3>
+        <div ref={ErrorRef}></div>
+        <form ref={FormRef} id="login">
+          <div className="inputElement">
+            <input
+              type="email"
+              className="inputElement-input"
+              placeholder="email"
+            />
+          </div>
+          <br />
+          <div className="inputElement">
+            <input
+              type="password"
+              className="inputElement-input"
+              placeholder="password"
+            />
+          </div>
+          <br />
+          <div className="inputElement">
+            <button className="inputElement-button">Login</button>
+          </div>
+          <br />
+        </form>
+        {/*<div className="clickable">Forgot password</div>//*/}
+        <br />
+      </div>
+    </Dialog>
+  )
+}
+
+const Register = (props: { Ref: React.RefObject<HTMLDialogElement> }) => {
+  const FormRef = React.useRef<HTMLFormElement>(null)
+  const ErrorRef = React.useRef<HTMLDivElement>(null)
+  React.useEffect(() => {
+    if (FormRef.current !== null) {
+      FormRef.current.addEventListener('submit', (e) => {
+        e.preventDefault()
+        handleRegister()
+      })
+    }
+  }, [])
+  const handleRegister = () => {
+    if (FormRef.current !== null && ErrorRef.current !== null) {
+      const form = (FormRef.current as unknown) as HTMLInputElement[]
+      const errorDiv = ErrorRef.current
+      if (form[1].value !== form[2].value) {
+        errorDiv.innerHTML = 'Passwords must match'
+        return
+      }
+      errorDiv.innerHTML = 'Signing up, please wait!'
+      ;(async () => {
+        let signUp = { user: null, message: null }
+
+        try {
+          //@ts-ignore
+          signUp = await firebase
+            .auth()
+            .createUserWithEmailAndPassword(form[0].value, form[1].value)
+        } catch (e) {
+          signUp = e
+        }
+        if ((signUp.user || '') === '') {
+          errorDiv.innerHTML = signUp.message || 'Error'
+        } else {
+          document.location.reload()
+        }
+      })()
+    }
+  }
+  return (
+    <Dialog title="Register" Ref={props.Ref}>
+      <div className="signUpForm">
+        <h3>Register</h3>
+        <div ref={ErrorRef}></div>
+        <form ref={FormRef} id="register">
+          <div className="inputElement">
+            <input
+              type="email"
+              className="inputElement-input"
+              placeholder="Email"
+            />
+          </div>
+          <br />
+          <div className="inputElement">
+            <input
+              type="password"
+              className="inputElement-input"
+              placeholder="Password"
+            />
+          </div>
+          <br />
+          <div className="inputElement">
+            <input
+              type="password"
+              className="inputElement-input"
+              placeholder="Repeat Password"
+            />
+          </div>
+          <br />
+          <div className="inputElement">
+            <button className="inputElement-button">Register</button>
+          </div>
+          <br />
+        </form>
+        <br />
+      </div>
+    </Dialog>
+  )
+}
+
+const Header = () => {
+  const logOut = () => {
+    ;(async () => {
+      //@ts-ignore
+      await firebase.auth().signOut()
+      window.location.reload()
+    })()
+  }
+  const LoginRef = React.useRef<HTMLDialogElement>(null)
+  const RegisterRef = React.useRef<HTMLDialogElement>(null)
+  const showModal = (element: React.RefObject<HTMLDialogElement>) => {
+    if (element.current !== null) {
+      element.current.showModal()
+    }
+  }
+  return (
+    <header className="header" id="header">
+      <div className="signIn">
+        {
+          //@ts-ignore
+          firebase.auth().currentUser === null ? (
+            <>
+              <span
+                className="clickable"
+                onClick={() => {
+                  showModal(LoginRef)
+                }}
+              >
+                Login
+              </span>{' '}
+              <span
+                className="clickable"
+                onClick={() => {
+                  showModal(RegisterRef)
+                }}
+              >
+                Register
+              </span>
+              <Login Ref={LoginRef} />
+              <Register Ref={RegisterRef} />
+            </>
+          ) : (
+            <>
+              <Link to="register">Favourites</Link>
+              <span className="clickable" onClick={logOut}>
+                Log Out
+              </span>
+            </>
+          )
+        }
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- react-client/src/index.tsx
+++ react-client/src/index.tsx
@@ -3,26 +3,31 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import * as serviceWorker from './serviceWorker'
 import Main from './Pages/main'
+import Header from './Components/Header'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
-ReactDOM.render(
-  <Router>
-    <React.StrictMode>
-      <Switch>
-        <Route path="/about">
-          <div>Hello</div>
-        </Route>
-        <Route path="/users">
-          <div>World</div>
-        </Route>
-        <Route path="/">
-          <Main />
-        </Route>
-      </Switch>
-    </React.StrictMode>
-  </Router>,
-  document.getElementById('root'),
-)
+//@ts-ignore
+firebase.auth().onAuthStateChanged((user) => {
+  ReactDOM.render(
+    <Router>
+      <React.StrictMode>
+        <Header />
+        <Switch>
+          <Route path="/about">
+            <div>Hello</div>
+          </Route>
+          <Route path="/users">
+            <div>World</div>
+          </Route>
+          <Route path="/">
+            <Main />
+          </Route>
+        </Switch>
+      </React.StrictMode>
+    </Router>,
+    document.getElementById('root'),
+  )
+})
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
