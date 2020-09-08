2e26cf6d0dedb2e13fa114e79a962d2236941d86
Andrius Simanaitis <eragonas5@gmail.com>
2020-09-08 10:58:51 +0300

    added navigation

---
+++ reactnative/screens/Second.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import {StackScreenProps} from '@react-navigation/stack';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+type RootStackParamList = {
+  Main: undefined;
+  Second: undefined;
+};
+
+type propTypes = StackScreenProps<RootStackParamList, 'Second'>;
+const Second = ({navigation}: propTypes) => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <View style={styles.sectionContainer}>
+            <Text
+              style={styles.sectionDescription}
+              onPress={() => navigation.navigate('Main')}>
+              Go to Main
+            </Text>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default Second;
