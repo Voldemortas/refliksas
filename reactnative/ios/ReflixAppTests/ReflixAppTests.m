01152e298abbc6dc051ad4e36971194eeda20440
Andrius Simanaitis <eragonas5@gmail.com>
2020-09-15 21:16:13 +0300

    changed app and package names

--- reactnative/ios/ReflixAppTests/ReflixAppTests.m
+++ reactnative/ios/ReflixAppTests/ReflixAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface reactnativeTests : XCTestCase
+@interface ReflixAppTests : XCTestCase
 
 @end
 
-@implementation reactnativeTests
+@implementation ReflixAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
